d50c4c58ad775fe9549d6a968064d1565b1d869f
gutardivo <gutardivo@me.com>
2023-10-25 17:55:45 -0300

    fix size iphone

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       width: {
-        'years': 'calc(36vw)'
+        'years': 'calc(38vw)'
       }
     },
   },
